4a1fc65e3ea0d92cfd60d760aafccdfb7d62828c
Ranjith Shenoy <ranjith.s@deltax.com>
2020-01-09 11:41:24 +0530

    Add scenario

---
+++ Oops.Test/AttendanceSteps.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using TechTalk.SpecFlow;
+
+namespace Oops.Test
+{
+    [Binding]
+    public class AttendanceSteps
+    {
+        [Given(@"I have a person with Id as ""(.*)""")]
+        public void GivenIHaveAPersonWithIdAs(int p0)
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [When(@"When I mark the person as present for ""(.*)""")]
+        public void WhenWhenIMarkThePersonAsPresentFor(string p0)
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [Then(@"the attendance for the person for the day should be ""(.*)""")]
+        public void ThenTheAttendanceForThePersonForTheDayShouldBe(string p0)
+        {
+            ScenarioContext.Current.Pending();
+        }
+    }
+}
